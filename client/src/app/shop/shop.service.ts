19e492310de105b47aadbcd427b19310b2d9f4d5
Hicham Benkirane <hicham@Hichams-Mac-mini.local>
2022-10-13 19:16:57 +0200

    add routing

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,10 +1,11 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {IPagination} from '../shared/models/Pagination';
-import {IBrand} from '../shared/models/brand';
-import {IType} from '../shared/models/productType';
-import {map} from 'rxjs/operators';
-import {ShopParams} from '../shared/models/shopParams';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { IPagination } from '../shared/models/Pagination';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +13,7 @@ import {ShopParams} from '../shared/models/shopParams';
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
@@ -34,7 +34,6 @@ export class ShopService {
     params = params.append('pageIndex', shopParams.pageNumber.toString());
     params = params.append('pageIndex', shopParams.pageSize.toString());
 
-
     return this.http
       .get<IPagination>(this.baseUrl + 'products', {
         observe: 'response',
@@ -45,6 +44,10 @@ export class ShopService {
           return response.body;
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
